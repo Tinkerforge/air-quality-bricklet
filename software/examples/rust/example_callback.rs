294523cd95bd7d869eb363212e146b7111c4cf7e
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:23:16 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for all values events.
-    let all_values_receiver = aq.get_all_values_receiver();
+    let all_values_receiver = aq.get_all_values_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `aq` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `aq` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for all_values in all_values_receiver {
